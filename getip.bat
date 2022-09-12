6bdb5dc5b48d1df4b46fea4cecedd582c91f2f92
wtain <ramiz.zeynalov@gmail.com>
2022-09-13 01:09:33 +0200

    Fixed IP discovery mechanism

--- getip.bat
+++ getip.bat
@@ -1,3 +1,3 @@
 @echo off
-for /f "tokens=3 delims=: " %%i  in ('netsh interface ip show config name^="Wi-Fi" ^| findstr "IP Address" ^| findstr [0-9]') do set IP=%%i
+for /f "tokens=3 delims=: " %%i  in ('netsh interface ip show config name^="Wi-Fi" ^| findstr "IP Address" ^| findstr [0-9]') do setx IP %%i /m
 echo %IP%
